1fda598b3774420f7f384675f04daa08c0269d29
Ra1NuX <ra1nux.01@gmail.com>
2022-01-17 01:24:21 +0100

    now data can be shared betweens subsites

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,22 +3,41 @@ import { useEffect, useState } from "react"
 import Dashboard from "./dashboard"
 import Chat from "./Chat"
 import Calendar from "./Calendar"
+import { auth } from "../firebase"
 
 export default function Content({...props}) {
     const {size} = props 
 
-    let [Oclass, setClass] = useState()
+    const [data , setData] = useState(null);
 
+    const connectToServer = async (id:string, server?:string) => {
+        try{
 
-    const handleDataChange = (data:any) => {
-        setClass(data) ;
+            const res = await fetch("http://127.0.0.1:3001/users-in", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Cors": "no-cors",
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify({
+                    userName: auth.currentUser?.displayName,
+                    userId: auth.currentUser?.uid,
+                    id, 
+                })
+            })
+            let resdata = await res.json();
+            if(data == null && resdata ) setData(resdata);
+        }catch(e){
+            console.log(e)
+        }
     }
 
-    return <div style={{marginLeft: size+10}}>
+    return <div style={{marginLeft: size+10 }}>
             <Routes>
-                <Route path="/chat" element={<Chat courses={Oclass}/>}/>
-                <Route path="/dashboard" element={<Dashboard onDataChange={handleDataChange}/>} />
-                <Route path="/calendar" element={<Calendar courses={Oclass}/>}/>
+                <Route path="/chat" element={<Chat courses={data}/>}/>
+                <Route path="/dashboard" element={<Dashboard data={data} cts={connectToServer}/>} />
+                <Route path="/calendar" element={<Calendar courses={data}/>}/>
                 <Route path="/drive" element={<Calendar />}/>
                 <Route path="/" element={<Navigate to="/dashboard"/>}/>
                 <Route path="*" element={<div>Esto es un error</div>} />
--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -5,72 +5,69 @@ import { auth, storage } from "../firebase"
 import { updateProfile } from "@firebase/auth"
 import { getDownloadURL, ref, uploadBytes } from "@firebase/storage"
 import "../styles/dashboard.css"
-    
 
-export default function Dashboard({...props}){
 
-    const {onDataChange} = props
+export default function Dashboard({ ...props }) {
 
-   
-    const [imgURL, setImageURL ] = useState("")
-    const [data , setData] = useState(null);
+    const { data, cts } = props
 
-    const connectToServer = async () => {
-        const res = await fetch("http://127.0.0.1:3001/users-in", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Cors": "no-cors",
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({
-                userName: auth.currentUser?.displayName,
-                userId: auth.currentUser?.uid,
-                id: "_p6dqd7wdb"
-            })
-        })
-        let resdata = await res.json();
-        if(data == null && resdata ) setData(resdata);
-    }
-    const handleData = () => {
-        if(data == undefined) return;
-        onDataChange(data)
-    }
+
+    const [imgURL, setImageURL] = useState("")
+
 
     useEffect(() => {
-        
-        auth.currentUser?.photoURL == null || auth.currentUser?.photoURL == undefined 
-            ? setImageURL("https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png") 
-            : setImageURL(auth.currentUser.photoURL) ; 
-        
-        connectToServer()
-        handleData();
 
-    },[])
+        auth.currentUser?.photoURL == null || auth.currentUser?.photoURL == undefined
+            ? setImageURL("https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png")
+            : setImageURL(auth.currentUser.photoURL);
 
-    const handleChangeFoto = async(e:any) => {
-        
-        if(e.target.files.length == 0) return;
+        cts("_ma2l4seqi"); // connect to server
+
+    }, [])
+
+    const handleChangeFoto = async (e: any) => {
+
+        if (e.target.files.length == 0) return;
         const profilePicsRef = ref(storage, `profilePics/${auth.currentUser?.displayName}-profile-pic.jpg`)
-        
-        await uploadBytes(profilePicsRef, e.target.files[0]); 
+
+        await uploadBytes(profilePicsRef, e.target.files[0]);
         const photoURL = await getDownloadURL(profilePicsRef);
-        
-        await updateProfile(auth.currentUser!, {photoURL});
+
+        await updateProfile(auth.currentUser!, { photoURL });
         setImageURL(auth.currentUser!.photoURL!);
 
     }
 
+    function dataCoruses() {
+        return <><input type="text" disabled value={data.roomName}/>
+        <h4>Tutor: </h4>
+        <input type="text" disabled value={"Jhon Doe Doe"} />
+        </>
+    }
 
-    const {size} = props
+
+    const { size } = props
     return <div>
-    <div className="NameAndFoto">
-        <input type="file" onChange={(e) => handleChangeFoto(e)} accept={".jpeg, .png, .jpg, .bmp, "} id="openProfilePicEditor" style={{display:"none"}} />
-        <label htmlFor="openProfilePicEditor" ><img src={`${imgURL}`} alt={`Foto de Perfil de ${auth.currentUser?.displayName}`} /></label>
-        <h1>Bienvenido {auth.currentUser?.displayName}</h1>
-    </div>
-    <h3>Cursos:</h3> {data == null ? "Actualmente no esta cursando ningun curso" : `Usted esta cursando: ${data.roomName}`} <button>Inscribirse</button>
-    <h3>Correo Electronico: </h3> {auth.currentUser?.email} <button>Editar </button>
-    {handleData()}
+        <div className="NameAndFoto">
+            <input type="file" onChange={(e) => handleChangeFoto(e)} accept={".jpeg, .png, .jpg, .bmp, "} id="openProfilePicEditor" style={{ display: "none" }} />
+            <label htmlFor="openProfilePicEditor" ><img src={`${imgURL}`} alt={`Foto de Perfil de ${auth.currentUser?.displayName}`} /></label>
+            <h1>Bienvenido {auth.currentUser?.displayName}</h1>
+        </div>
+        <h3 className="Group-Title">Información del Curso</h3>
+        <h4>Denominación: </h4>
+
+        {data == null
+            ? "Actualmente no esta cursando ningun curso"
+            :  dataCoruses()
+        }
+
+        
+        <h3 className="Group-Title">Información Personal</h3>
+        <h4>Nombre Completo: </h4>
+        <input type="text" disabled value={"Jhon Doe Doe"} />
+        <h4>Edad: </h4>
+        <input type="number" disabled value={18} />
+        <h4>Correo Electronico: </h4>
+        {<input type="email" disabled value={auth.currentUser?.email + ""} />}
     </div>
 }

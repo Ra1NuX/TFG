736518408491d3054d4fb357c5c3f7836b43afb9
Ra1NuX <57793739+Ra1NuX@users.noreply.github.com>
2022-07-07 20:27:17 +0200

    Update firebase.ts
    delete firebase-config

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import {initializeApp} from "firebase/app";
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB9Esun1j-uTBUU4TDv5LJDZUrQRkGnX5I",
-    authDomain: "bbdd-tfg-ec27a.firebaseapp.com",
-    projectId: "bbdd-tfg-ec27a",
-    storageBucket: "bbdd-tfg-ec27a.appspot.com",
-    messagingSenderId: "739311376686",
-    appId: "1:739311376686:web:3d53838a76b1074bedc2bf"
+    apiKey: /*<API-KEY>*/,
+    authDomain: /*authDomain*/,
+    projectId: /*projectId*/,
+    storageBucket: /*storageBucket*/
+    messagingSenderId: /*messagingSenderId*/
+    appId: /*appId*/
 };
 
 initializeApp(firebaseConfig);
